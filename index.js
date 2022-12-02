6d938bfebf239e4b3559f0caa37f2e0a9266149b
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2022-12-02 12:39:07 -0500

    commit tree 6ec2c43c31c866e4154953c4706b3442bc4f28cc

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(userName, userEmail){
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+          },
+          body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+          }),
+        };
+
+return fetch("http://localhost:3000/users", configObj)
+.then(function(resp){
+    return resp.json();
+})
+.then(function(data){
+    const id = data.id;
+    return document.body.append(data.id);
+})
+.catch(function (error) {
+    alert("UH OH! Something went wrong!")
+    return document.body.append(error.message);
+  });
+}
